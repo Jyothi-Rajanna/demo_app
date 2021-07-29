bee60ecc58b3c23ed5b1b350ae849c6159eb2441
Jyothi-Rajanna <jyothirajanna@gmail.com>
2021-07-29 12:42:55 +0530

    test by changing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        My first react app deployment
+        My first react app going live
       </header>
     </div>
   );
